b9da746c852d3c185d9f4b7bbf1929d83fb028cd
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2024-02-05 11:32:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -54,3 +54,118 @@ class _appState extends State<app> {
     );
   }
 }
+
+//  q.2
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const app());
+}
+
+class app extends StatefulWidget {
+  const app({super.key});
+
+  @override
+  State<app> createState() => _appState();
+}
+
+class _appState extends State<app> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('THE WALL',style: TextStyle(
+              color: Colors.white,
+            ),),
+            backgroundColor: Color(0xff212121),
+          ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  l1(),
+                  l2(),
+                  l1(),
+                  l2(),
+                  l1(),
+                  l2(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+Widget l1()
+{
+  return Row(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 5,bottom: 5),
+        height: 90,
+        width: 130,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ),
+      SizedBox(width: 10),
+      Container(
+        margin: EdgeInsets.only(top: 5,bottom: 5),
+        height: 90,
+        width: 180,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ),
+      SizedBox(width: 10),
+      Container(
+        margin: EdgeInsets.only(top: 5,bottom: 5),
+        height: 90,
+        width: 160,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ),
+    ],
+  );
+}
+Widget l2()
+{
+  return Row(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 5,bottom: 5),
+        height: 90,
+        width: 160,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ),
+      SizedBox(width: 10),
+      Container(
+        margin: EdgeInsets.only(top: 5,bottom: 5),
+        height: 90,
+        width: 120,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ),
+      SizedBox(width: 10),
+      Container(
+        margin: EdgeInsets.only(top: 5,bottom: 5),
+        height: 90,
+        width: 190,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ),
+    ],
+  );
+}
